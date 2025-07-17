d28562efd2aa7f2c8b8cf28457e270488f62439b
Hexenone <101699955+Hexenone@users.noreply.github.com>
2025-07-17 12:56:23 +0700

    1

---
+++ my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import './Background/Background.module.css';
+
+function App() {
+  return (
+    <>
+      <div className="movingPattern">
+        <h1>Haracter Qreation</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
